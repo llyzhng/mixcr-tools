09ddefcc80278e1781d1a3d497623127406ba4f0
llyzhng <lzheng163@gmail.com>
2022-11-08 14:11:57 -0500

    add parameters for count thresholds

--- scripts/collapse-clone-tables-by-VDJ-CDR3.R
+++ scripts/collapse-clone-tables-by-VDJ-CDR3.R
@@ -7,7 +7,11 @@ option_list = list(
   make_option(c("-i", "--infile"), type="character", default=NULL,
               help="path for clone table to collapse", metavar="character"),
   make_option(c("-o", "--outfile"), type="character", default=NULL,
-              help="path for output combined tabled", metavar="character")
+              help="path for output combined tabled", metavar="character"),
+  make_option(c("-u", "--umi_threshold"), type="numeric", default=0,
+              help="minimum uniqueUMICount to keep clone", metavar="numeric"),
+  make_option(c("-r", "--read_threshold"), type="numeric", default=0,
+              help="minimum readCount to keep clone", metavar="numeric")
 )
 
 opt_parser = OptionParser(option_list=option_list);
@@ -34,11 +38,19 @@ if (!hasGeneCols) {
 x$VDJ <- paste(x$bestVGene, x$bestDGene, x$bestJGene, sep = "|")
 x$nSeqCDR3[is.na(x$nSeqCDR3)] <- ""
 
-# use UMI if available 
+ 
 if (any(grepl("uniqueUMI", colnames(x)))) {
+  # use UMI if available
   collapsed_x <- aggregate(cbind(uniqueUMICount, uniqueUMIFraction) ~ VDJ + nSeqCDR3, data = x, FUN=sum)
+  collapsed_x <- collapsed_x[uniqueUMICount > opt$umi_threshold, ]
+  new_sum <- sum(collapsed_x$uniqueUMICount)
+  collapsed_x$cloneFraction <- collapsed_x$uniqueUMICount / new_sum
 } else {
+  # use read count
   collapsed_x <- aggregate(cbind(readCount, readFraction) ~ VDJ + nSeqCDR3, data = x, FUN=sum)
+  collapsed_x <- collapsed_x[readCount > opt$read_threshold, ]
+  new_sum <- sum(collapsed_x$readCount)
+  collapsed_x$cloneFraction <- collapsed_x$readCount / new_sum
 }
 colnames(collapsed_x)[4] <- "cloneFraction"
 
